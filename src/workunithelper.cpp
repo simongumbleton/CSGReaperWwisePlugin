63761ccb0c39084cdd30e1b79145d0ca0b639677
simongumbleton <simongumbleton@gmail.com>
2021-10-21 22:49:19 +0100

    adding work unit xml lib

---
+++ src/workunithelper.cpp
@@ -0,0 +1,10 @@
+//
+//  workunithelper.cpp
+//  reaper_wwise_csg
+//
+//  Created by Simon Gumbleton on 21/10/2021.
+//  Copyright © 2021 My Company. All rights reserved.
+//
+
+#include <stdio.h>
+#include "workunithelper.h"
