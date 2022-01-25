c687ec9e5355b4f65820f67c04ff7563037736c0
simongumbleton <simongumbleton@gmail.com>
2022-01-25 23:32:56 +0000

    start settings window

---
+++ include/gui_settings.h
@@ -0,0 +1,31 @@
+#include "JUCE/JuceHeader.h"
+#include "GUI.h"
+#include "reaperHelpers.h"
+#include "platformhelpers.h"
+
+struct SettingsStruct
+{
+	
+};
+
+class SettingsWindow : public DocumentWindow
+{
+	
+};
+
+
+
+class TransferSettingsWnd : public SettingsWindow
+{
+	
+};
+
+class MetadataSettingsWnd : public SettingsWindow
+{
+	
+};
+
+class EDLSettingsWnd : public SettingsWindow
+{
+	
+};
---
+++ src/gui_settings.cpp
@@ -0,0 +1,9 @@
+//
+//  gui_settings.cpp
+//  reaper_wwise_csg
+//
+//  Created by Simon Gumbleton on 25/01/2022.
+//  Copyright © 2022 My Company. All rights reserved.
+//
+
+#include "gui_settings.h"
