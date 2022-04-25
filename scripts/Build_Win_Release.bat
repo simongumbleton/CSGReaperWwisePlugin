f6f27f45bbf0d0364ae8e0ea11a0225a55a419f6
Simon Gumbleton <ext.simon.gumbleton@arrowheadgs.net>
2022-04-25 08:28:48 +0100

    Update Build_Win_Release.bat

--- scripts/Build_Win_Release.bat
+++ scripts/Build_Win_Release.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv" ../csgreaperwwiseplugin.sln /Build Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv" ../csgreaperwwiseplugin.sln /Build Release
 pause
